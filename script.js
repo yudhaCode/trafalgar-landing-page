ff13c93601363053203a0adef493c458361b0ada
yudhaCode <yudhasaurus@gmail.com>
2021-10-22 06:18:55 +0700

    trafalgar

---
+++ script.js
@@ -0,0 +1,28 @@
+  //Get the button
+  var mybutton = document.getElementById("myBtn");
+  
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+// toggle menu
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar')[0]
+console.log(navbarLinks)
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
